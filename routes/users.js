0c7723dba89924a8fb1c0d4dd54cd6034c37f807
Rhenaldo Arya <rhenaldoaryap@gmail.com>
2022-02-27 08:29:34 +0700

    serving uploaded files to website visitors

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const multer = require("multer");
 
+// get access to the database
+const db = require("../data/database");
+
 // storing the file with original name and keep the extensions of the file.
 const storageConfig = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -14,8 +17,10 @@ const storageConfig = multer.diskStorage({
 const upload = multer({ storage: storageConfig });
 const router = express.Router();
 
-router.get("/", function (req, res) {
-  res.render("profiles");
+router.get("/", async function (req, res) {
+  // get access to database and fetching all of data inside of it in ARRAY to be able work with it
+  const users = await db.getDb().collection("users").find().toArray();
+  res.render("profiles", { users: users });
 });
 
 router.get("/new-user", function (req, res) {
@@ -24,13 +29,17 @@ router.get("/new-user", function (req, res) {
 
 // the order of the routes, multer and function matters
 // upload.single need the name of the input file, look what name attribute you used at the input element that hold the file you want to upload
-router.post("/profiles", upload.single("image"), function (req, res) {
+router.post("/profiles", upload.single("image"), async function (req, res) {
   // get access to the file will be upload
   const uploadedImageFile = req.file;
   const userData = req.body;
 
-  console.log(uploadedImageFile);
-  console.log(userData);
+  // get access to the database and configuring what file that should be stored in database
+  await db.getDb().collection("users").insertOne({
+    name: userData.username,
+    // not storing the image to database, but storing the path
+    imagePath: uploadedImageFile.path,
+  });
 
   res.redirect("/");
 });
