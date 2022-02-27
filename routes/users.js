75b8e52c2e732c761f6b28378d02a416a0d850db
Rhenaldo Arya <rhenaldoaryap@gmail.com>
2022-02-27 07:47:44 +0700

    configuring multer in-depth

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const multer = require("multer");
 
-// storing the file to specific folder we target to
-const upload = multer({});
+// storing the file with original name and keep the extensions of the file.
+const storageConfig = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "images");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storageConfig });
 const router = express.Router();
 
 router.get("/", function (req, res) {
@@ -13,12 +22,17 @@ router.get("/new-user", function (req, res) {
   res.render("new-user");
 });
 
-// the order of the target link, multer and function matters
-// upload.single need the name of the input file, look what name attribute you use at the input element that hold the file you want to upload
+// the order of the routes, multer and function matters
+// upload.single need the name of the input file, look what name attribute you used at the input element that hold the file you want to upload
 router.post("/profiles", upload.single("image"), function (req, res) {
   // get access to the file will be upload
   const uploadedImageFile = req.file;
   const userData = req.body;
+
+  console.log(uploadedImageFile);
+  console.log(userData);
+
+  res.redirect("/");
 });
 
 module.exports = router;
